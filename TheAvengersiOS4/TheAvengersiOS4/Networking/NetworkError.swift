3b27c115b52d3689d0213e3b4d46201798b415cf
Matthew Hill <matthill265@gmail.com>
2023-03-01 14:36:53 -0700

    NetworkError file completed

--- TheAvengersiOS4/TheAvengersiOS4/Networking/NetworkError.swift
+++ TheAvengersiOS4/TheAvengersiOS4/Networking/NetworkError.swift
@@ -8,6 +8,22 @@
 import Foundation
 
 enum NetworkError: LocalizedError {
-    case invalidURL
+case invalidURL
+case thrownError(Error)
+case noData
+case unableToDecode
     
+    var errorDescription: String? {
+        switch self {
+            
+        case .invalidURL:
+            return "Invalid URL. Check URL endpoint."
+        case .thrownError(let error):
+            return "Thrown error. Error was \(error.localizedDescription)"
+        case .noData:
+            return "No data received from the network fetch."
+        case .unableToDecode:
+            return "Unable to decode model object from data."
+        }
+    }
 }
